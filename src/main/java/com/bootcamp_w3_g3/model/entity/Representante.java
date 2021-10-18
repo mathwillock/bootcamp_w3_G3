671153553d67e05e51bbeec5b2aebfb09e9da7b1
lexscruz <alex.cruz@mercadolivre.com>
2021-10-18 16:48:44 -0300

    Efetuado criacão do entity representante, com referencia de autor.

--- src/main/java/com/bootcamp_w3_g3/model/entity/Representante.java
+++ src/main/java/com/bootcamp_w3_g3/model/entity/Representante.java
@@ -3,6 +3,12 @@ package com.bootcamp_w3_g3.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+
+/**
+ *
+ * @Autor Alex Cruz
+ */
+
 @Getter
 @Setter
 public class Representante extends Pessoa{

a93b101764b1343823fc23750f853964a2c74031
mark-oliveira-ml <89139884+mark-oliveira-ml@users.noreply.github.com>
2021-10-18 19:07:14 -0300

    classe dimensão anotada, Enum TipoProduto.

--- src/main/java/com/bootcamp_w3_g3/model/entity/TipoProduto.java
+++ src/main/java/com/bootcamp_w3_g3/model/entity/TipoProduto.java
@@ -1,7 +1,7 @@
 package com.bootcamp_w3_g3.model.entity;
 
 /**@author Marcelo de Oliveira Santos
- * 
+ *
  * Enum utilizado para a asserção dos tres tipos de produtos especificados:
  * CONGELADOS FRESCOS E REFRIGERADOS
  * @version 0.0.1

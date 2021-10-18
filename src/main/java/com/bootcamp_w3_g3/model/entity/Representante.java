0a30316662073a911921d2faa396ae174a25df0a
lexscruz <alex.cruz@mercadolivre.com>
2021-10-18 16:21:03 -0300

    Efetuado criacão do entity representante.

---
+++ src/main/java/com/bootcamp_w3_g3/model/entity/Representante.java
@@ -0,0 +1,21 @@
+package com.bootcamp_w3_g3.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Representante extends Pessoa{
+
+    Integer codigo;
+
+    public Representante(String nome, String sobrenome, String cpf, String telefone, String endereco) {
+        super(nome, sobrenome, cpf, telefone, endereco);
+    }
+
+    public Representante() {
+    }
+
+
+
+}

b134dbf880886488e8db7a13bd6c2a233b514ea8
joaquimborges <joaquim.borges1993@gmail.com>
2021-10-19 18:40:10 -0300

    Advice e handler para auxiliar no tratamento de erros da criacao e busca de um lote

---
+++ src/main/java/com/bootcamp_w3_g3/advisor/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package com.bootcamp_w3_g3.advisor;
+
+/**
+ * @author Joaquim Borges
+ */
+
+public class EntityNotFoundException extends RuntimeException{
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}

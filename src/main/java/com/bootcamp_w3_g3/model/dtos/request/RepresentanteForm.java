f65379619418248c5e7e9d2fc348d5cceb886821
lexscruz <alex.cruz@mercadolivre.com>
2021-10-20 01:11:07 -0300

    Inclusão do @Getter Alex

--- src/main/java/com/bootcamp_w3_g3/model/dtos/request/RepresentanteForm.java
+++ src/main/java/com/bootcamp_w3_g3/model/dtos/request/RepresentanteForm.java
@@ -2,6 +2,7 @@ package com.bootcamp_w3_g3.model.dtos.request;
 
 import com.bootcamp_w3_g3.model.entity.Lote;
 import com.bootcamp_w3_g3.model.entity.Representante;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
@@ -12,6 +13,7 @@ import java.time.LocalDate;
  *
  * @Autor Alex Cruz
  */
+@Getter
 public class RepresentanteForm {
 
     private String nome;

7106cdde718586f9a950cec4ab66b0cccac0257f
joaquimborges <joaquim.borges1993@gmail.com>
2021-10-27 09:03:16 -0300

    primento endpoint finalizado, registrar uma ordem de entrada.

--- src/main/java/com/bootcamp_w3_g3/controller/OrdemEntradaController.java
+++ src/main/java/com/bootcamp_w3_g3/controller/OrdemEntradaController.java
@@ -1,11 +1,17 @@
 package com.bootcamp_w3_g3.controller;
 
 
+import com.bootcamp_w3_g3.model.dtos.request.OrdemDeEntradaDTO;
+import com.bootcamp_w3_g3.model.entity.OrdemDeEntrada;
 import com.bootcamp_w3_g3.service.OrdemDeEntradaService;
 import com.bootcamp_w3_g3.service.RepresentanteService;
 import com.bootcamp_w3_g3.service.SetorService;
 import com.bootcamp_w3_g3.service.VendedorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Matheus Willock
  * @author Alex Cruz
  * @author Hugo Damm
- * @author Marcelo Oliveira
+ * @author Marcelo Santos
  */
 @RestController
 @RequestMapping("api/ordem-entrada/")
 public class OrdemEntradaController {
 
     @Autowired
-    private OrdemDeEntradaService ordemDeEntradaService;
+    private OrdemDeEntradaService ordemService;
 
     @Autowired
     private VendedorService vendedorService;
@@ -33,6 +39,14 @@ public class OrdemEntradaController {
 
     @Autowired
     private SetorService setorService;
+
+
+    @PostMapping("/registrar")
+    public ResponseEntity<OrdemDeEntradaDTO> registrarOrdem(@RequestBody OrdemDeEntradaDTO dto) {
+        OrdemDeEntrada ordemDeEntrada = dto.converte(setorService, representanteService, vendedorService);
+        ordemService.registra(ordemDeEntrada);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
 
 }
--- src/main/java/com/bootcamp_w3_g3/service/OrdemDeEntradaService.java
+++ src/main/java/com/bootcamp_w3_g3/service/OrdemDeEntradaService.java
@@ -2,10 +2,13 @@ package com.bootcamp_w3_g3.service;
 
 
 import com.bootcamp_w3_g3.model.entity.Armazem;
+import com.bootcamp_w3_g3.model.entity.OrdemDeEntrada;
 import com.bootcamp_w3_g3.model.entity.Representante;
 import com.bootcamp_w3_g3.model.entity.Setor;
+import com.bootcamp_w3_g3.repository.OrdemDeEntradaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 /**
  * @author Matheus Willock
@@ -16,18 +19,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrdemDeEntradaService {
+    private OrdemDeEntradaRepository ordemDeEntradaRepository;
 
     private final RepresentanteService representanteService;
-    private final VendedorService vendedorService;
+    private final ArmazemService armazemService;
     private final SetorService setorService;
 
 
     @Autowired
-    public OrdemDeEntradaService(RepresentanteService representanteService, VendedorService vendedorService,
+    public OrdemDeEntradaService(OrdemDeEntradaRepository ordemDeEntradaRepository,
+                                 RepresentanteService representanteService, ArmazemService armazemService,
                                  SetorService setorService) {
 
         this.representanteService = representanteService;
-        this.vendedorService = vendedorService;
+        this.armazemService = armazemService;
         this.setorService = setorService;
 
     }
@@ -36,7 +41,7 @@ public class OrdemDeEntradaService {
      * @autor Joaquim Borges
      * metodo para validar o armazem
      */
-    private boolean validarArmazem(String codigoArmazem) {
+    private boolean armazemExiste(String codigoArmazem) {
         for (Setor setor : setorService.listarSetores()) {
             if (setor.getArmazem().getCodArmazem().equals(codigoArmazem)){
                 Armazem armazem = setor.getArmazem();
@@ -76,11 +81,54 @@ public class OrdemDeEntradaService {
     }
 
 
+    /**
+     * @autor Matheus Willock
+     * metodo para validar a existencia do setor no armazem
+     */
+    private boolean setorExiste(String codigo) {
+        for (Setor setor : armazemService.listarSetores()) {
+            if (setor.getCodigo().equals(codigo)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @autor Marcelo Santos
+     */
+    private boolean cabeNoSetor (Integer quantidadeAtual, String codSetor){
+        if (setorService.obterSetor(codSetor) == null ) {
+            return false;
+        }
+        return   (armazemService.listarSetores().stream()
+                .filter(s-> s.getCodigo().equals(codSetor))
+                        .findFirst()).get().getEspacoDisponivel() >= quantidadeAtual;
+
+    }
+
+    /**
+     * @author Matheus Willock
+     * @author Joaquim Borges
+     * @author Alex Cruz
+     * @author Marcelo Santos
+     * @author Hugo Damm
+     */
+    private boolean validarOrdem (OrdemDeEntrada ordemDeEntrada) {
+        return armazemExiste(ordemDeEntrada.getSetor().getArmazem().getCodArmazem()) &&
+                representantePertenceAoArmazem(ordemDeEntrada.getRepresentante().getCodigo()) &&
+                setorExiste(ordemDeEntrada.getSetor().getCodigo()) &&
+                setorCorrespondeAoTipoDeProduto(ordemDeEntrada.getSetor().getTipoProduto()) &&
+                cabeNoSetor(ordemDeEntrada.getSetor().getEspacoDisponivel(), ordemDeEntrada.getSetor().getCodigo());
+    }
 
 
-
-
-
+    public OrdemDeEntrada registra(OrdemDeEntrada ordemDeEntrada) {
+        if (validarOrdem(ordemDeEntrada)) {
+            return ordemDeEntradaRepository.save(ordemDeEntrada);
+        }
+        return null;
+    }
 
 
 }

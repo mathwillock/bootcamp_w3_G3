9dd7eef3fc8b6656c27f0a120c29d559add97e51
Matheus Willock <matheuswillock@gmail.com>
2021-11-05 10:29:37 -0300

    Commit:    - Finish: class LoteUnitTest;    - Refactor:        - class LoteService;        - class ProdutoService;

--- src/main/java/com/bootcamp_w3_g3/service/LoteService.java
+++ src/main/java/com/bootcamp_w3_g3/service/LoteService.java
@@ -30,20 +30,16 @@ public class LoteService {
     @Autowired
     private SetorService setorService;
 
-
     @Autowired
     public LoteService(LoteRepository loteRepository) {
         this.loteRepository = loteRepository;
     }
 
-
     @Transactional
     public Lote salvar(Lote lote) {
 
-
         Integer numeroLote = lote.getNumero();
         lote.getProduto().setCodLote(numeroLote);
-
         lote.setSetor(lote.getSetor());
         lote.setProduto(lote.getProduto());
 
@@ -76,9 +72,6 @@ public class LoteService {
         return loteRepository.save(editedLote);
     }
 
-    public Lote apagar(Integer numeroDoLote) {
-        return loteRepository.deleteByNumero(numeroDoLote);
-    }
 
 
 

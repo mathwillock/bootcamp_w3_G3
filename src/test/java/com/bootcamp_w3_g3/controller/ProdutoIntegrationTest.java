567c07b962b149d8ffbb1e9de7b36553cb8f2114
Hugo Damm <hugo2008rj@hotmail.com>
2021-11-17 16:22:46 -0300

    Teste Integração do 2o endpoint - Requisito 5 feito.
    Adicionado autor Hugo no LoteUnitTest.

--- src/test/java/com/bootcamp_w3_g3/controller/ProdutoIntegrationTest.java
+++ src/test/java/com/bootcamp_w3_g3/controller/ProdutoIntegrationTest.java
@@ -176,6 +176,17 @@ public class ProdutoIntegrationTest {
                 ;
     }
 
+    private ProdutoForm payloadProduto13() {
+        return ProdutoForm.builder()
+                .codigoDoProduto(85)
+                .nome("Peixes")
+                .preco(50.0)
+                .tipoProduto(TipoProduto.FRESCOS)
+                .temperaturaIndicada(5.0)
+                .build()
+                ;
+    }
+
     private RepresentanteForm payloadRepresentante(){
         return RepresentanteForm.builder()
                 .codigo("R-30")
@@ -206,6 +217,17 @@ public class ProdutoIntegrationTest {
                 .endereco("rua qualquer")
                 .cpf("123.234.345-04")
                 .telefone("11-2473648")
+                .build();
+    }
+
+    private RepresentanteForm payloadRepresentante5(){
+        return RepresentanteForm.builder()
+                .codigo("R-105")
+                .nome("Marcelo")
+                .sobrenome("Souza")
+                .endereco("Rua A")
+                .cpf("345.123.678-04")
+                .telefone("11-24473648")
                 .build();
     }
 
@@ -251,6 +273,18 @@ public class ProdutoIntegrationTest {
         return ArmazemForm.builder()
                 .codArmazem("AR-102")
                 .nome("armazem central")
+                .representante(representanteForm)
+                .endereco("qualquer lugar")
+                .numero(100)
+                .uf("SP").build();
+    }
+
+    private ArmazemForm payloadArmazem5(RepresentanteForm representanteForm) {
+        this.persisteRepresentante(representanteForm);
+
+        return ArmazemForm.builder()
+                .codArmazem("AR-105")
+                .nome("Armazem São Paulo")
                 .representante(representanteForm)
                 .endereco("qualquer lugar")
                 .numero(100)
@@ -599,6 +633,56 @@ public class ProdutoIntegrationTest {
         persisteLote2(loteForm2);
 
         this.mockMvc.perform(get("http://localhost:8080/produtos/lotes/validade/" + setorForm.getCodigo() + "/20" ))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * Teste deve listar todos os lotes por prazo de validade
+     * que pertencem a uma determinada categoria de produto
+     * @author Hugo Damm
+     */
+    @Test
+    void deveListarLotesValidadeCategoria() throws Exception{
+        ProdutoForm produtoForm13 = payloadProduto13();
+        this.persisteProduto(produtoForm13);
+
+        RepresentanteForm representanteForm5 = payloadRepresentante5();
+        ArmazemForm armazemForm5 = payloadArmazem5(representanteForm5);
+        this.persisteArmazem(armazemForm5);
+
+        SetorForm setorForm5 = SetorForm.builder()
+                .codigo("S-105")
+                .tipoProduto(TipoProduto.FRESCOS)
+                .nome("C")
+                .armazem(armazemForm5)
+                .build();
+
+        this.persisteSetor1(setorForm5);
+
+        LoteForm loteForm3 = LoteForm.builder()
+                .numero(104)
+                .produtoForm(produtoForm13)
+                .setorForm(setorForm5)
+                .temperaturaAtual(14.2)
+                .temperaturaMinima(11.0)
+                .dataDeFabricacao(LocalDate.now()).dataDeValidade(LocalDate.of(2021, 9, 01))
+                .quantidadeAtual(10)
+                .build();
+
+        LoteForm loteForm4 = LoteForm.builder()
+                .numero(105)
+                .produtoForm(produtoForm13)
+                .setorForm(setorForm5)
+                .temperaturaAtual(14.2)
+                .temperaturaMinima(11.0)
+                .dataDeFabricacao(LocalDate.now()).dataDeValidade(LocalDate.of(2021, 9, 20))
+                .quantidadeAtual(10)
+                .build();
+
+        persisteLote2(loteForm3);
+        persisteLote2(loteForm4);
+
+        this.mockMvc.perform(get("http://localhost:8080/produtos/lotes/validade?tipoProduto=" + setorForm5.getTipoProduto() + "&dias=45" ))
                 .andExpect(status().isOk());
     }
 
--- src/test/java/com/bootcamp_w3_g3/service/LoteUnitTest.java
+++ src/test/java/com/bootcamp_w3_g3/service/LoteUnitTest.java
@@ -22,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * @author Joaquim Borges
  * @author Matheus Willock
+ * @author Hugo Damm
  */
 
 public class LoteUnitTest {

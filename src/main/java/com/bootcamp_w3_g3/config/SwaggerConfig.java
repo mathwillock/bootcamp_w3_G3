96726b1311705aa8925cc4a02e8223be76215ba9
Hugo Damm <hugo2008rj@hotmail.com>
2021-11-23 13:36:28 -0300

    SwaggerConfig, comentários retirados.

--- src/main/java/com/bootcamp_w3_g3/config/SwaggerConfig.java
+++ src/main/java/com/bootcamp_w3_g3/config/SwaggerConfig.java
@@ -2,42 +2,20 @@ package com.bootcamp_w3_g3.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.RequestMethod;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
-//@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
-//    private List<ResponseMessage> responseMessageForGET(){
-//        return new ArrayList<>() {{
-//            add(new ResponseMessageBuilder()
-//                    .code(500)
-//                    .message("500 message")
-//                    .responseModel(new ModelRef("Error"))
-//                    .build());
-//            add(new ResponseMessageBuilder()
-//                    .code(403)
-//                    .message("Forbidden!")
-//                    .build());
-//        }};
-//    }
 
     @Bean
     public Docket api() {
@@ -47,8 +25,6 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(getApiInfo());
-//                .useDefaultResponseMessages(false)
-//                .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
     }
 
     private ApiInfo getApiInfo() {
